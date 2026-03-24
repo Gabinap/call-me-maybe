024229f4349c092d78074b4af0126a3a8d0b8d01
Gabin Agulhon--Pavageau <gagulhon@z3r4p4.42lyon.fr>
2026-03-24 14:23:06 +0100

    feat: try for multi prompt

--- src/__main__.py
+++ src/__main__.py
@@ -1,15 +1,29 @@
-from .parsing import parse, Args
 from llm_sdk import Small_LLM_Model
+
+from .parsing import parse, Args
+
+
+def get_vocabulary(model_instance: Small_LLM_Model) -> dict[str, int]:
+    vocab_file_path = model_instance.get_path_to_vocab_file()
+    with open(vocab_file_path) as f:
+        vocab = {line.strip(): i for i, line in enumerate(f)}
+    return vocab
 
 
 def gingembre(args: Args, model: str) -> None:
     model_instance = Small_LLM_Model(model_name=model)
-    for prompt in args.prompts:
+    vocab: dict[str, int] = get_vocabulary(model_instance)
+    reverse_vocab: dict[int, str] = {v: k for k, v in vocab.items()}
+    decoded: str = ""
+    for prompt in args.prompts[:1]:
         encoded = model_instance.encode(prompt)
-        logits = model_instance.get_logits_from_input_ids(encoded[0].tolist())
-        answer = max(logits)
-        decoded = model_instance.decode([int(answer)])
-        print(decoded)
+        while '\'' or '\"' not in decoded:
+            logits = model_instance.get_logits_from_input_ids(encoded[0].tolist())
+            best_token = logits.index(max(logits))
+            token_id: int = best_token if best_token in reverse_vocab else 0
+            decoded = model_instance.decode([token_id])
+            print(decoded)
+            encoded = encoded + [token_id]
 
 
 def main(model: str = "Qwen/Qwen3-0.6B") -> None:
